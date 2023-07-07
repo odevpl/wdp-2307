d363611941678e641b5d13ced9b5aa7796d9cc02
Maciej Laskowski <maciek30088@gmail.com>
2023-07-07 23:07:23 +0100

    Add Responsive styles to top bar and banner

--- src/components/layout/TopBar/TopBar.js
+++ src/components/layout/TopBar/TopBar.js
@@ -33,12 +33,14 @@ const TopBar = () => (
           <ul>
             <li>
               <a href='#'>
-                <FontAwesomeIcon className={styles.icon} icon={faUser} /> Login
+                <FontAwesomeIcon className={styles.icon} icon={faUser} />
+                <span className={styles.iconText}>Login</span>
               </a>
             </li>
             <li>
               <a href='#'>
-                <FontAwesomeIcon className={styles.icon} icon={faLock} /> Register
+                <FontAwesomeIcon className={styles.icon} icon={faLock} />
+                <span className={styles.iconText}>Register</span>
               </a>
             </li>
             <li>
