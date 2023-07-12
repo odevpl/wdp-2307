1eda6b2128b03429b21966d2bf2a67cf4e7ddf7c
Marcin Poręba <marcin.poreba1995@gmail.com>
2023-07-13 00:40:44 +0200

    Add Comments component with actives dots and avatars

--- src/components/features/Comments/Comments.js
+++ src/components/features/Comments/Comments.js
@@ -1,69 +1,90 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faQuoteRight } from '@fortawesome/free-solid-svg-icons';
 import styles from './Comments.module.scss';
 
-class Comments extends React.Component {
-  state = {
-    comments: [
-      { id: 1, author: 'John', content: 'Great product!', avatar: 'john.jpg' },
-      { id: 2, author: 'Jane', content: 'I love it!', avatar: 'jane.jpg' },
-      { id: 3, author: 'Mike', content: 'Highly recommended.', avatar: 'mike.jpg' },
-    ],
-    activeComment: 1,
+const Comments = () => {
+  const [comments] = useState([
+    {
+      id: 1,
+      author: 'John',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+      avatar: 'john.jpg',
+    },
+    {
+      id: 2,
+      author: 'Jane',
+      content:
+        'felis. Sed neque arcu, tincidunt eu lorem eget, vehicula dignissim urna.',
+      avatar: 'jane.jpg',
+    },
+    {
+      id: 3,
+      author: 'Mike',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+      avatar: 'mike.jpg',
+    },
+  ]);
+
+  const [activeComment, setActiveComment] = useState(comments[0].id);
+  const [activeCommentData, setActiveCommentData] = useState(comments[0]);
+
+  const handlePageChange = commentId => {
+    setActiveComment(commentId);
+    const newActiveCommentData = comments.find(comment => comment.id === commentId);
+    setActiveCommentData(newActiveCommentData);
   };
 
-  handlePageChange = commentId => {
-    this.setState({ activeComment: commentId });
-  };
-
-  render() {
-    const { comments, activeComment } = this.state;
-    const activeCommentData = comments.find(comment => comment.id === activeComment);
-
-    return (
-      <div className={styles.root}>
-        <div className='container'>
-          <div className={styles.feedback}>
-            <div className={styles.header}>
-              <h3>CLIENT FEEDBACK</h3>
-              <div>
-                <ul className={styles.switch}>
-                  {comments.map(comment => (
-                    <li key={comment.id}>
-                      <a
-                        onClick={() => this.handlePageChange(comment.id)}
-                        className={`${styles.dot} ${
-                          activeComment === comment.id ? styles.activeDot : ''
-                        }`}
-                      ></a>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            </div>
-            <div className={styles.contents}>
-              <ul>
-                {activeCommentData && (
-                  <li className={styles.active}>
-                    <div className={styles.commentContainer}>
-                      <div className={styles.avatar}>
-                        <img
-                          src={activeCommentData.avatar}
-                          alt={activeCommentData.author}
-                        />
-                      </div>
-                      <div className={styles.commentContent}>
-                        {activeCommentData.content}
-                      </div>
-                    </div>
+  return (
+    <div className={styles.root}>
+      <div className='container'>
+        <div className={styles.feedback}>
+          <div className={styles.header}>
+            <h3>CLIENT FEEDBACK</h3>
+            <div>
+              <ul className={styles.switch}>
+                {comments.map(comment => (
+                  <li key={comment.id}>
+                    <a
+                      onClick={() => handlePageChange(comment.id)}
+                      className={`${styles.dot} ${
+                        activeComment === comment.id ? styles.activeDot : ''
+                      }`}
+                    ></a>
                   </li>
-                )}
+                ))}
               </ul>
             </div>
           </div>
+          <div className={styles.contents}>
+            <ul>
+              {activeCommentData && (
+                <li className={styles.active}>
+                  <div className={styles.commentContainer}>
+                    <div className={styles.client}>
+                      <h4>{activeCommentData.author}</h4>
+                      <div className={styles.avatar}>
+                        <img
+                          src={`/images/comments/${activeCommentData.avatar}`}
+                          alt={activeCommentData.author}
+                        />
+                      </div>
+                    </div>
+                    <div className={styles.commentContent}>
+                      {activeCommentData.content}
+                    </div>
+                    <div className={styles.quoteIcon}>
+                      <FontAwesomeIcon icon={faQuoteRight} />
+                    </div>
+                  </div>
+                </li>
+              )}
+            </ul>
+          </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Comments;
