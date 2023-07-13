669e013e53c14d863e200216b3db945f1b5426c6
Maciej Laskowski <maciek30088@gmail.com>
2023-07-13 17:49:27 +0100

    Uncomment changes in NewFurniture.js

--- src/components/features/NewFurniture/NewFurniture.js
+++ src/components/features/NewFurniture/NewFurniture.js
@@ -100,13 +100,13 @@ class NewFurniture extends React.Component {
             </div>
           </Swipeable>
 
-          {/* <div className='row'>
+          <div className='row'>
             {categoryProducts.slice(activePage * 8, (activePage + 1) * 8).map(item => (
               <div key={item.id} className='col-3'>
                 <ProductBox {...item} />
               </div>
             ))}
-          </div> */}
+          </div>
           {compared > 0 ? <ComparedProductsBox /> : ''}
         </div>
       </div>
