0c16219b43a92b969ae2824aa3d1751dcd1eee55
Maciej Laskowski <maciek30088@gmail.com>
2023-07-13 13:55:16 +0100

    Add remaining styles

--- src/components/features/ProductData/ProductData.js
+++ src/components/features/ProductData/ProductData.js
@@ -13,9 +13,14 @@ import {
   faPinterest,
   faTwitter,
   faLinkedin,
-
 } from '@fortawesome/free-brands-svg-icons';
-import { faPlus, faMinus, faShoppingBasket, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import {
+  faPlus,
+  faMinus,
+  faSearchPlus,
+  faShoppingBasket,
+  faEnvelope,
+} from '@fortawesome/free-solid-svg-icons';
 import { faExchangeAlt } from '@fortawesome/free-solid-svg-icons';
 import { faStar as farStar, faHeart } from '@fortawesome/free-regular-svg-icons';
 import { useDispatch } from 'react-redux';
@@ -41,132 +46,178 @@ const ProductData = () => {
     dispatch(toggleFavorite(product.id));
   };
   return (
-    <div>
-      <div>
-        <div className='row'>
-          <div className='col-md-5'>
-            <img src={`/${product.picture}`} alt={product.name} className='img-fluid' />
+    <div className={styles.root}>
+      <div className='row'>
+        <div className='col-md-5'>
+          <div className={styles.image}>
+            <img
+              src={`/${product.picture}`}
+              alt={product.name}
+              className={styles.image}
+            />
+            <Button className={styles.zoomButton} variant='outline'>
+              <FontAwesomeIcon icon={faSearchPlus} />
+            </Button>
           </div>
-          <div className='col-md-7'>
-            <div className='row'>
-              <h5 className='col-11'>{product.name}</h5>
-              <div className='col-1'>
-                <div className={styles.outlines}>
-                  <Button>&lt;</Button>
-                  <Button>&gt;</Button>
-                </div>
-              </div>
-            </div>
-            <div className='row reviews border-bottom my-2'>
-              <div className='col-2'>
-                <ProductStars
-                  stars={product.stars}
-                  // myStars={selectedStars}
-                  // onClick={handleStarClick}
-                />
-              </div>
-              <div className='col-1'>
-                <p>(0 Reviews)</p>
-              </div>
-              <div className='col-1'>
-                <p>|</p>
-              </div>
-              <div className='col-2'>
-                <p>Add your review</p>
-              </div>
-            </div>
-            <div className='row border-bottom my-2 py-3'>
-              <div className='col-4 '>
-                <div className={styles.pricesContainer}>
-                  {product.oldPrice && (
-                    <p className={styles.oldPrice}> $ {product.oldPrice} </p>
-                  )}
-                  <div className={styles.price}>
-                    <Button noHover variant={isHovering ? 'price' : 'small'}>
-                      $ {product.price}
-                    </Button>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className='row my-2'>
+          <div className={` ${styles.imageSmallGallery}`}>
+            <img
+              src={`/${product.picture}`}
+              alt={product.name}
+              className={styles.imageSmall}
+            />
+            <img
+              src={`/${product.picture}`}
+              alt={product.name}
+              className={styles.imageSmall}
+            />
+            <img
+              src={`/${product.picture}`}
+              alt={product.name}
+              className={styles.imageSmall}
+            />
+            <img
+              src={`/${product.picture}`}
+              alt={product.name}
+              className={styles.imageSmall}
+            />
+            <Button className={styles.arrowButtonLeft} variant='outline'>
+              &lt;
+            </Button>
+            <Button className={styles.arrowButtonRight} variant='outline'>
+              &gt;
+            </Button>
+          </div>
+        </div>
+        <div className='col-md-7'>
+          <div className='row'>
+            <h4 className='col-10'>{product.name}</h4>
+            <div className='col-2'>
               <div className={styles.outlines}>
-                <Button variant='outline'>
-                  <FontAwesomeIcon icon={faShoppingBasket}></FontAwesomeIcon> ADD TO
-                  CART
+                <Button className={styles.arrowButton} variant='outline'>
+                  &lt;
                 </Button>
-                <Button
-                  variant={product.isFavorite ? 'active' : 'outline'}
-                  onClick={favoriteHandler}
-                >
-                  <FontAwesomeIcon icon={faHeart}>Favorite</FontAwesomeIcon>
-                </Button>
-                <Button
-                  variant={Math.floor(Math.random() * 2) === 1 ? 'outline' : 'active'}
-                >
-                  <FontAwesomeIcon icon={faExchangeAlt}>Add to compare</FontAwesomeIcon>
-                </Button>
-                <Button variant='outline'>
-                  <FontAwesomeIcon icon={faEnvelope}></FontAwesomeIcon>
+                <Button className={styles.arrowButton} variant='outline'>
+                  &gt;
                 </Button>
               </div>
             </div>
-            <div className='row my-2 border-bottom'>
-              <strong className='col-2'>Quantity:</strong>
-              <div className='col-10'>
-                <Button variant='outline'> 2 </Button>
-                <Button variant='outline'>
-                  <FontAwesomeIcon icon={faMinus}>-</FontAwesomeIcon>
-                </Button>
-                <Button variant='outline'>
-                  <FontAwesomeIcon icon={faPlus}>+</FontAwesomeIcon>
-                </Button>
+          </div>
+          <div className={`col-6 p-0 my-2 ${styles.reviews}`}>
+            <div>
+              <ProductStars
+                stars={product.stars}
+                // myStars={selectedStars}
+                // onClick={handleStarClick}
+              />
+            </div>
+            <div>
+              <p>(0 Reviews)</p>
+            </div>
+            <div>
+              <p>|</p>
+            </div>
+            <div>
+              <p>Add your review</p>
+            </div>
+          </div>
+          <div className='row border-top border-bottom my-2 py-3'>
+            <div className='col-4 '>
+              <div className={styles.pricesContainer}>
+                {product.oldPrice && (
+                  <p className={styles.oldPrice}> $ {product.oldPrice} </p>
+                )}
+                <div className={styles.price}>
+                  <Button noHover variant={isHovering ? 'price' : 'small'}>
+                    $ {product.price}
+                  </Button>
+                </div>
               </div>
             </div>
-            <div className='row border-bottom'>
-              <p className='col-12'>
-                <strong>Quick Overview</strong>
-              </p>
-              <p className='col-12'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit esse tempor
-                incididunt ut labore et dolore magna aliqua
-              </p>
+          </div>
+
+          <div className={`row my-2 ${styles.actionButtons}`}>
+            <div className='col-12'>
+              <Button className={styles.addToCartButton} variant='outline'>
+                <FontAwesomeIcon icon={faShoppingBasket}></FontAwesomeIcon> ADD TO CART
+              </Button>
+
+              <Button
+                className={styles.actionButton}
+                variant={product.isFavorite ? 'active' : 'outline'}
+                onClick={favoriteHandler}
+              >
+                <FontAwesomeIcon icon={faHeart}>Favorite</FontAwesomeIcon>
+              </Button>
+              <Button
+                className={styles.actionButton}
+                variant={Math.floor(Math.random() * 2) === 1 ? 'outline' : 'active'}
+              >
+                <FontAwesomeIcon icon={faExchangeAlt}>Add to compare</FontAwesomeIcon>
+              </Button>
+              <Button className={styles.actionButton} variant='outline'>
+                <FontAwesomeIcon icon={faEnvelope}></FontAwesomeIcon>
+              </Button>
             </div>
-            <div className='row'>
-              <p className='col-3'>
-                <strong>Availability:</strong>
-              </p>
-              <p className='col-9'>In stock</p>
+          </div>
+
+          <div className='row py-2 my-2 border-bottom'>
+            <strong className='col-2'>Quantity:</strong>
+            <div className='col-10'>
+              <Button className={styles.quantityButton} variant='outline'>
+                {' '}
+                2{' '}
+              </Button>
+              <Button className='mx-1' variant='outline'>
+                <FontAwesomeIcon icon={faMinus}>-</FontAwesomeIcon>
+              </Button>
+              <Button variant='outline'>
+                <FontAwesomeIcon icon={faPlus}>+</FontAwesomeIcon>
+              </Button>
             </div>
-            <div className='row border-bottom'>
-              <p className='col-3'>
-                <strong>Category:</strong>
-              </p>
-              <p className='col-9'>{product.category}</p>
-            </div>
-            <div className='row'>
-              <div className='col-12 socialButtons'>
-                <button className='socialButton'>
-                  <FontAwesomeIcon icon={faFacebook} className='socialIcon' />
-                  Facebook
-                </button>
-                <button className='socialButton'>
-                  <FontAwesomeIcon icon={faGooglePlus} className='socialIcon' />
-                  Google+
-                </button>
-                <button className='socialButton'>
-                  <FontAwesomeIcon icon={faPinterest} className='socialIcon' />
-                  Pinterest
-                </button>
-                <button className='socialButton'>
-                  <FontAwesomeIcon icon={faTwitter} className='socialIcon' />
-                  Twitter
-                </button>
-                <button className='socialButton'>
-                  <FontAwesomeIcon icon={faLinkedin} className='socialIcon' />
-                  LinkedIn
-                </button>
-              </div>
+          </div>
+          <div className='row border-bottom'>
+            <p className='col-12'>
+              <strong>Quick Overview</strong>
+            </p>
+            <p className='col-12'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit esse tempor
+              incididunt ut labore et dolore magna aliqua
+            </p>
+          </div>
+          <div className='row'>
+            <p className='col-2'>
+              <strong>Availability:</strong>
+            </p>
+            <p className='col-9'>In stock</p>
+          </div>
+          <div className='row border-bottom'>
+            <p className='col-2'>
+              <strong>Category:</strong>
+            </p>
+            <p className='col-9'>{product.category}</p>
+          </div>
+          <div className='row py-3'>
+            <div className={`col-12 ${styles.socialButtons}`}>
+              <button className={styles.Facebook}>
+                <FontAwesomeIcon icon={faFacebook} className={styles.socialIcon} />
+                <p>Share</p>
+              </button>
+              <button className={styles.Google}>
+                <FontAwesomeIcon icon={faGooglePlus} className={styles.socialIcon} />
+                <p>Google+</p>
+              </button>
+              <button className={styles.Pinterest}>
+                <FontAwesomeIcon icon={faPinterest} className={styles.socialIcon} />
+                <p>Pinterest</p>
+              </button>
+              <button className={styles.Twitter}>
+                <FontAwesomeIcon icon={faTwitter} className={styles.socialIcon} />
+                <p>Tweet</p>
+              </button>
+              <button className={styles.LinkedIn}>
+                <FontAwesomeIcon icon={faLinkedin} className={styles.socialIcon} />
+                <p>LinkedIn</p>
+              </button>
             </div>
           </div>
         </div>
