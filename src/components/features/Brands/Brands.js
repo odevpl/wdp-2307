836b5c3648b725cf1026cd8092b2952cdfd245fc
GabrielaDun <123959816+GabrielaDun@users.noreply.github.com>
2023-07-15 11:04:38 +0200

    WDP-32 RWD for brands section

--- src/components/features/Brands/Brands.js
+++ src/components/features/Brands/Brands.js
@@ -9,41 +9,44 @@ const Brands = () => {
           <div className={styles.row}>
             <button>&#60;</button>
             <div className={styles.logos}>
-              <img
-                className={styles.logo}
-                alt='Logo'
-                src={'/images/logos/logo-1.jpg'}
-              ></img>
-              <img
-                className={styles.logo}
-                alt='Logo'
-                src={'/images/logos/logo-2.jpg'}
-              ></img>
-              <img
-                className={styles.logo}
-                alt='Logo'
-                src={'/images/logos/logo-3.jpg'}
-              ></img>
-              <img
-                className={styles.logo}
-                alt='Logo'
-                src={'/images/logos/logo-4.jpg'}
-              ></img>
-              <img
-                className={styles.logo}
-                alt='Logo'
-                src={'/images/logos/logo-5.jpg'}
-              ></img>
-              <img
-                className={styles.logo}
-                alt='Logo'
-                src={'/images/logos/logo-6.jpg'}
-              ></img>
-              <img
-                className={styles.logo}
-                alt='Logo'
-                src={'/images/logos/logo-7.jpg'}
-              ></img>
+              <div className={styles.logoBox}>
+                <img
+                  className={styles.logo}
+                  alt='Logo'
+                  src={'/images/logos/logo-1.jpg'}
+                ></img>
+                <img
+                  className={styles.logo}
+                  alt='Logo'
+                  src={'/images/logos/logo-2.jpg'}
+                ></img>
+              </div>
+              <div className={`d-none d-md-none d-lg-block ${styles.logoBox}`}>
+                <img
+                  className={styles.logo}
+                  alt='Logo'
+                  src={'/images/logos/logo-3.jpg'}
+                ></img>
+                <img
+                  className={styles.logo}
+                  alt='Logo'
+                  src={'/images/logos/logo-4.jpg'}
+                ></img>
+              </div>
+              <div
+                className={`d-none d-sm-block d-sm-none d-md-block ${styles.logoBox}`}
+              >
+                <img
+                  className={styles.logo}
+                  alt='Logo'
+                  src={'/images/logos/logo-6.jpg'}
+                ></img>
+                <img
+                  className={styles.logo}
+                  alt='Logo'
+                  src={'/images/logos/logo-7.jpg'}
+                ></img>
+              </div>
             </div>
             <button>&#62;</button>
           </div>
