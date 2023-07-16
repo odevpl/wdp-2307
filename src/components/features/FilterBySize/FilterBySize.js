a55b7cec364b65fd5368dcd6d151b8a8de146e7c
GabrielaDun <123959816+GabrielaDun@users.noreply.github.com>
2023-07-16 13:27:12 +0200

    Start the functionality structure

---
+++ src/components/features/FilterBySize/FilterBySize.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import styles from './FilterBySize.module.scss';
+import clsx from 'clsx';
+
+const FilterBySize = () => {
+  return (
+    <div className={styles.root}>
+      <div className={styles.title}>FILTER BY CATEGORY</div>
+      <div>
+        <button className={styles.categories}>
+          <div className={clsx(styles.categoryBox, styles.active)}>
+            <div className={styles.categorBoxTwo}>
+              <input className='checkbox' type='checkbox' checked='checked' />
+              <div className={styles.category}> S</div>
+            </div>
+            <div className={styles.number}>3</div>
+          </div>
+        </button>
+        <button className={styles.categories}>
+          <div className={styles.categoryBox}>
+            <div className={styles.categorBoxTwo}>
+              <input type='checkbox' />
+              <div className={styles.category}> M</div>
+            </div>
+            <div className={styles.number}>4</div>
+          </div>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default FilterBySize;
