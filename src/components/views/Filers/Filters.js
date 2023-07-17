06f4d648b6b0af4444a59af7201d7a1a9b6ffd33
Michal Sak <michusakowicz@gmail.com>
2023-07-17 13:19:32 +0200

    WDP-63 add colours

--- src/components/views/Filers/Filters.js
+++ src/components/views/Filers/Filters.js
@@ -2,10 +2,12 @@ import React from 'react';
 import styles from './Filters.module.scss';
 import FilterByCategory from '../../features/FilterByCategory/FilterByCategory';
 import FilterBySize from '../../features/FilterBySize/FilterBySize';
+import FilterByColors from '../../features/FilterByColors/FilterByColors';
 
 const Filters = () => {
   return (
     <div className={styles.roots}>
+      <FilterByColors />
       <FilterByCategory />
       <FilterBySize />
     </div>
