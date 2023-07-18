a65a6ff58abc483e36bb8ece9d27b9be601a8a74
Marcin Poręba <marcin.poreba1995@gmail.com>
2023-07-18 20:02:08 +0200

    fix problem with banner

--- src/components/views/ProductList/ProductList.js
+++ src/components/views/ProductList/ProductList.js
@@ -4,7 +4,8 @@ import { useParams } from 'react-router-dom';
 import styles from './ProductList.module.scss';
 import Brands from '../../features/Brands/Brands';
 import Filters from '../Filers/Filters';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
 import NewFurniture from '../../features/NewFurniture/NewFurnitureContainer';
 
 const ProductList = () => {
@@ -13,8 +14,38 @@ const ProductList = () => {
   return (
     <div className={styles.root}>
       <div className='container'>
-        <div className='row'>
-          <div className='col-12'>Banner</div>
+        <div className={styles.root}>
+          <div className={styles.ProjectNew}>
+            <div className={styles.upSection}>
+              <div className='row'>
+                <div className='col-12'>
+                  <div className={styles.upText}>
+                    <p className={styles.styleBedroom}>BEDROOM</p>
+                    <p className={styles.styleFur}>FURNITURE</p>
+                  </div>
+                </div>
+                <div className='col-12'>
+                  <div className={styles.downText}>
+                    <p className={styles.alwaysText}>ALWAYS</p>
+                    <p className={styles.procentText}>25%</p>
+                    <p className={styles.moreText}>OFF OR MORE</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className='row'>
+                <div className='col-12'>
+                  <div className={styles.downSection}></div>
+                  <div className={styles.positionHome}>
+                    <p className={styles.homeText}>Home</p>
+                    <FontAwesomeIcon className={styles.icon} icon={faChevronRight} />
+                    <p className={styles.furText}>Furniture</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
 
