18e6ecb771cd555c7986c8961c7a51fe4faa41c2
Michal Sak <michusakowicz@gmail.com>
2023-07-20 18:01:57 +0200

    WDP-81 Add empty pages, INFORMATION

---
+++ src/components/views/ConditionsPage/ConditionsPage.js
@@ -0,0 +1,12 @@
+import styles from '../ConditionsPage/ConditionsPage.module.scss';
+import React from 'react';
+
+const ConditionsPage = () => {
+  return (
+    <div className={styles.ConditionsPage}>
+      <h1>ConditionsPage</h1>
+    </div>
+  );
+};
+
+export default ConditionsPage;
