eec35d49388c34c6364ce38f4cec0e3c59aca8bd
Marcin Poręba <marcin.poreba1995@gmail.com>
2023-07-21 12:09:56 +0200

    Solving the problem with not navigating to home, added spacing between icons for better readability solving the problem with the products not being rendered on the chair table subpage, etc Solving the problem with cart icon

--- src/components/views/ProductList/ProductList.js
+++ src/components/views/ProductList/ProductList.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-//import PropTypes from 'prop-types';
 import styles from './ProductList.module.scss';
 import Brands from '../../features/Brands/Brands';
 import Filters from '../Filters/Filters';
-
 import NewFurniture from '../../features/NewFurniture/NewFurnitureContainer';
 import Baner from '../../common/Baner/Baner';
 
@@ -14,33 +12,42 @@ const ProductList = () => {
   return (
     <div className={styles.root}>
       <div className='container'>
-        <div className='row'>
-          <div className='col-12'>
-            <Baner />
+        <div className={styles.root}>
+          <div className='row'>
+            <div className='col-12'>
+              <Baner />
+            </div>
           </div>
         </div>
-      </div>
-
-      <div className='container'>
-        <div className='row'>
-          <div className='col-9'>
-            {categoryId === 'furniture' ? <NewFurniture role='internal' /> : ''}
-          </div>
-          <div className='col-3'>
-            <Filters />
+        <div className='container'>
+          <div className='row'>
+            <div className='col-9'>
+              {categoryId !== 'home' && categoryId !== 'blog' && (
+                <NewFurniture
+                  role='internal'
+                  activeCategory={
+                    categoryId === 'furniture' || categoryId === 'bedroom'
+                      ? 'bed'
+                      : categoryId
+                  }
+                  key={categoryId}
+                />
+              )}
+            </div>
+            <div className='col-3'>
+              <Filters />
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className='container'>
-        <div className='row'>
-          <Brands />
+        <div className='container'>
+          <div className='row'>
+            <Brands />
+          </div>
         </div>
       </div>
     </div>
   );
 };
-
-// ProductList.propTypes = {};
 
 export default ProductList;
