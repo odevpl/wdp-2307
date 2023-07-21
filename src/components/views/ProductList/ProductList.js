6afa5eca911554aedb7e05407a2209df81471f23
Marcin Poręba <marcin.poreba1995@gmail.com>
2023-07-21 11:46:41 +0200

    fix problem with double rendering banner

--- src/components/views/ProductList/ProductList.js
+++ src/components/views/ProductList/ProductList.js
@@ -15,44 +15,12 @@ const ProductList = () => {
     <div className={styles.root}>
       <div className='container'>
         <div className={styles.root}>
-          <div className={styles.ProjectNew}>
-            <div className={styles.upSection}>
-              <div className='row'>
-                <div className='col-12'>
-                  <div className={styles.upText}>
-                    <p className={styles.styleBedroom}>BEDROOM</p>
-                    <p className={styles.styleFur}>FURNITURE</p>
-                  </div>
-                </div>
-                <div className='col-12'>
-                  <div className={styles.downText}>
-                    <p className={styles.alwaysText}>ALWAYS</p>
-                    <p className={styles.procentText}>25%</p>
-                    <p className={styles.moreText}>OFF OR MORE</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div>
-              <div className='row'>
-                <div className='col-12'>
-                  <div className={styles.downSection}></div>
-                  <div className={styles.positionHome}>
-                    <p className={styles.homeText}>Home</p>
-                    <FontAwesomeIcon className={styles.icon} icon={faChevronRight} />
-                    <p className={styles.furText}>Furniture</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
           <div className='row'>
             <div className='col-12'>
               <Baner />
             </div>
           </div>
         </div>
-
         <div className='container'>
           <div className='row'>
             <div className='col-9'>
